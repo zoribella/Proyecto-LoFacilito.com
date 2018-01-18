c2f6b6fb801652aa26b39915c51c4271d7a717cf
zoribel <zoribella23@gmail.com>
2018-01-18 14:47:42 -0300

    Se cambiaron las vistas del login y se unifico los estilos css

--- LoFacilito.com/app/routes.js
+++ LoFacilito.com/app/routes.js
@@ -13,8 +13,12 @@ module.exports = (app, passport)=>{
     //app.post('/login', (req, res) => ());
 
     app.get('/registro', (req, res)=>{
-        res.render('registro', { selected: { registro: true}})
+        res.render('registro', { selected: {registro: true} }, {
+            message: req.flash('signupMessage')
+        });
     });
+
+    //app.post('/registro', (req, res) => ());
     
     app.get('/detalle', (req, res)=>{
         res.render('detalle')
